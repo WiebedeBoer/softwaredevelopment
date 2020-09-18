263b7ae853ba7501c69041922ceac44bb66cff57
janekvw <janekvanwolfswinkel@hotmail.com>
2020-09-18 12:20:28 +0200

    Simulator bijgevoegd

---
+++ SimulatorTest/SimulatorTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimulatorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
