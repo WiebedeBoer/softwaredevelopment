8e14f707564cb1c77e2e62dece10ed81d6a58292
janekvw <janekvanwolfswinkel@hotmail.com>
2020-09-28 11:56:32 +0200

    Added Test

---
+++ Simulator/Simulator/RegularTrafficLight.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simulator
+{
+    public enum RegLightSequence
+    {
+        Green,
+        Yellow,
+        Red
+    }
+
+    public class RegularTrafficLight
+    {
+        const int delayedLightChange = 500; // in milliseconds
+        RegLightSequence currentColor = RegLightSequence.Red;
+
+        // Switch trafficlight to yellow, after a delay switch to green
+        public void SwitchLightToGreen()
+        {
+
+        }
+
+        // Switch trafficlight to yellow, after a delay switch to red
+        public void SwitchLightToRed()
+        {
+
+        }
+    }
+}
