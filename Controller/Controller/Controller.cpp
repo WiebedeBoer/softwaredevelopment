9fcf655d1c18acbafc28616c5a750f7b05d28b78
WiebedeBoer <wiebe81@gmail.com>
2020-10-09 12:08:24 +0200

    socket client

--- Controller/Controller/Controller.cpp
+++ Controller/Controller/Controller.cpp
@@ -10,8 +10,10 @@
 //#include <sys/socket.h> 
 //#include <arpa/inet.h> 
 //#include <unistd.h> 
+//#include <netinet/in.h> 
 #include "socket.h"
 #include "json_message.h"
+#include <stdlib.h> 
 //rapid json library
 #include "rapidjson\document.h"
 #include "rapidjson\writer.h"
@@ -202,8 +204,102 @@ public:
 	//https://github.com/ebshimizu/socket.io-clientpp
 	*/
 
+	//https://www.geeksforgeeks.org/socket-programming-cc/
+	//https://www.bogotobogo.com/cplusplus/sockets_server_client.php
+	int receivepackage(const char* hello) {
+
+		tcp_client_t client("127.0.0.1", 54000);
+		//values
+		int server_fd, new_socket, valread;
+		struct sockaddr_in address;
+		int opt = 1;
+		int addrlen = sizeof(address);
+		char buffer[1024] = { 0 };
+		//char* hello = "Hello from server";
+		int PORT = 54000;
+		int SO_REUSEPORT = 54000;
+
+		// Creating socket file descriptor 
+		if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+		{
+			perror("socket failed");
+			exit(EXIT_FAILURE);
+		}
+
+		// Forcefully attaching socket to the port 8080 
+		//if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+		if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, hello, sizeof(opt)))
+		{
+			perror("setsockopt");
+			exit(EXIT_FAILURE);
+		}
+		address.sin_family = AF_INET;
+		address.sin_addr.s_addr = INADDR_ANY;
+		address.sin_port = htons(PORT);
+
+		// Forcefully attaching socket to the port 8080 
+		if (bind(server_fd, (struct sockaddr*)&address,
+			sizeof(address)) < 0)
+		{
+			perror("bind failed");
+			exit(EXIT_FAILURE);
+		}
+		if (listen(server_fd, 3) < 0)
+		{
+			perror("listen");
+			exit(EXIT_FAILURE);
+		}
+		if ((new_socket = accept(server_fd, (struct sockaddr*)&address,
+			(socklen_t*)&addrlen)) < 0)
+		{
+			perror("accept");
+			exit(EXIT_FAILURE);
+		}
+		//valread = read(new_socket, buffer, 1024);
+		printf("%s\n", buffer);
+		send(new_socket, hello, strlen(hello), 0);
+		printf("Hello message sent\n");
+		return 0;
+
+	}
+
+	int sendpackage(const char* hello) {
+		int sock = 0, valread;
+		int PORT = 54000;
+		struct sockaddr_in serv_addr;
+		//char* hello = "Hello from client";
+		char buffer[1024] = { 0 };
+		if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		{
+			printf("\n Socket creation error \n");
+			return -1;
+		}
+
+		serv_addr.sin_family = AF_INET;
+		serv_addr.sin_port = htons(PORT);
+
+		// Convert IPv4 and IPv6 addresses from text to binary form 
+		if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+		{
+			printf("\nInvalid address/ Address not supported \n");
+			return -1;
+		}
+
+		if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
+		{
+			printf("\nConnection Failed \n");
+			return -1;
+		}
+		send(sock, hello, strlen(hello), 0);
+		printf("Hello message sent\n");
+		//valread = read(sock, buffer, 1024);
+		printf("%s\n", buffer);
+		return 0;
+	}
+
 	/*
-	void fetchjson() {
+	
+		void receivepackage() {
 		//tcp client
 		tcp_client_t client("127.0.0.1", 2000);
 		//request
@@ -231,6 +327,7 @@ public:
 		std::cout << "next_year: " << next_year << std::endl;
 		client.close();
 	}
+	
 	*/
 
 	/*
