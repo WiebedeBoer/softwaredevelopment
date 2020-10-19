7a8bdee62e51c4828bc9d3158c675270ca602e8d
janekvw <janekvanwolfswinkel@hotmail.com>
2020-10-19 12:11:35 +0200

    Changes to listener

---
+++ Simulator/Simulator/Path.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simulator
+{
+    class Path
+    {
+        public List<Node> nodes = new List<Node>();
+
+        public void addNode(int left, int top, RegularTrafficLight reg = null)
+        {
+            Node node = new Node(left, top, reg);
+
+            nodes.Add(node);
+        }
+    }
+}
