9065e160374eed1e90ea8a79384affa20219e5d0
WiebedeBoer <wiebe81@gmail.com>
2020-10-20 15:02:39 +0200

    win sock, str relace for backslash packet, parser needs fixing for single lane

--- Controller/Controller/Controller.cpp
+++ Controller/Controller/Controller.cpp
@@ -19,6 +19,8 @@
 #include "rapidjson\writer.h"
 #include "rapidjson\stringbuffer.h"
 #include <ctime>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
 using namespace rapidjson;
 
@@ -166,7 +168,26 @@ public:
 			trafficnorthsouthbus bus;
 			traffic = bus.traffic;
 		}
+		//traffic = std::replace(traffic.begin(), traffic.end(), \, );
+		//traffic.erase(std::remove(traffic.begin(), traffic.end(), \), traffic.end());
+		traffic = Replace(traffic, "\\\"", "\"");
+		//traffic = traffic.erase(std::remove(traffic.begin(), traffic.end(), '\\'), traffic.end());
 		return traffic;
+	}
+
+	std::string Replace(std::string str,
+		const std::string& oldStr,
+		const std::string& newStr)
+	{
+
+		size_t index = str.find(oldStr);
+		while (index != str.npos)
+		{
+			str = str.substr(0, index) +
+				newStr + str.substr(index + oldStr.size());
+			index = str.find(oldStr, index + newStr.size());
+		}
+		return str;
 	}
 
 	static rapidjson::Document fetchjson(const char* json) {
@@ -351,6 +372,157 @@ public:
 		printf("%s\n", buffer);
 		return 0;
 	}
+
+
+	void socketserver(std::string userInput)
+	{
+		std::string ipAddress = "127.0.0.1";			// IP Address of the server
+		int port = 54000;						// Listening port # on the server
+
+		// Initialize WinSock
+		WSAData data;
+		WORD ver = MAKEWORD(2, 2);
+		int wsResult = WSAStartup(ver, &data);
+		if (wsResult != 0)
+		{
+			//cerr << "Can't start Winsock, Err #" << wsResult << endl;
+			return;
+		}
+
+		// Create socket
+		SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+		if (sock == INVALID_SOCKET)
+		{
+			//cerr << "Can't create socket, Err #" << WSAGetLastError() << endl;
+			WSACleanup();
+			return;
+		}
+
+		// Fill in a hint structure
+		sockaddr_in hint;
+		hint.sin_family = AF_INET;
+		hint.sin_port = htons(port);
+		inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+
+		// Connect to server
+		int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));
+		if (connResult == SOCKET_ERROR)
+		{
+			//cerr << "Can't connect to server, Err #" << WSAGetLastError() << endl;
+			closesocket(sock);
+			WSACleanup();
+			return;
+		}
+
+		// Do-while loop to send and receive data
+		char buf[4096];
+		//std::string userInput;
+
+		do
+		{
+			// Prompt the user for some text
+			//cout << "> ";
+			//getline(cin, userInput);
+
+			if (userInput.size() > 0)		// Make sure the user has typed in something
+			{
+				// Send the text
+				int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
+				if (sendResult != SOCKET_ERROR)
+				{
+					// Wait for response
+					ZeroMemory(buf, 4096);
+					int bytesReceived = recv(sock, buf, 4096, 0);
+					if (bytesReceived > 0)
+					{
+						// Echo response to console
+						//cout << "SERVER> " << string(buf, 0, bytesReceived) << endl;
+					}
+				}
+			}
+
+		} while (userInput.size() > 0);
+
+		// Gracefully close down everything
+		closesocket(sock);
+		WSACleanup();
+	}
+
+	std::string socketclient(std::string userInput)
+	{
+		std::string ipAddress = "127.0.0.1";			// IP Address of the server
+		int port = 54000;						// Listening port # on the server
+
+		// Initialize WinSock
+		WSAData data;
+		WORD ver = MAKEWORD(2, 2);
+		int wsResult = WSAStartup(ver, &data);
+		if (wsResult != 0)
+		{
+			//cerr << "Can't start Winsock, Err #" << wsResult << endl;
+			return;
+		}
+
+		// Create socket
+		SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+		if (sock == INVALID_SOCKET)
+		{
+			//cerr << "Can't create socket, Err #" << WSAGetLastError() << endl;
+			WSACleanup();
+			return;
+		}
+
+		// Fill in a hint structure
+		sockaddr_in hint;
+		hint.sin_family = AF_INET;
+		hint.sin_port = htons(port);
+		inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+
+		// Connect to server
+		int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));
+		if (connResult == SOCKET_ERROR)
+		{
+			//cerr << "Can't connect to server, Err #" << WSAGetLastError() << endl;
+			closesocket(sock);
+			WSACleanup();
+			return;
+		}
+
+		// Do-while loop to send and receive data
+		char buf[4096];
+		//std::string userInput;
+
+		do
+		{
+			// Prompt the user for some text
+			//cout << "> ";
+			//getline(cin, userInput);
+
+			if (userInput.size() > 0)		// Make sure the user has typed in something
+			{
+				// Send the text
+				int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
+				if (sendResult != SOCKET_ERROR)
+				{
+					// Wait for response
+					ZeroMemory(buf, 4096);
+					int bytesReceived = recv(sock, buf, 4096, 0);
+					if (bytesReceived > 0)
+					{
+						// Echo response to console
+						//cout << "SERVER> " << string(buf, 0, bytesReceived) << endl;
+					}
+				}
+			}
+
+		} while (userInput.size() > 0);
+
+		// Gracefully close down everything
+		closesocket(sock);
+		WSACleanup();
+	}
+
+
 
 	/*
 	
